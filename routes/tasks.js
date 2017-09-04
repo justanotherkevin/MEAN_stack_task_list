0c13672d979255851955e6a4721c3ac9cd0f5272
kevin hu <hu.kevin.w@gmail.com>
2017-09-04 17:02:56 -0400

    Add route for save and show one task

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const mongojs = require('mongojs');
 
-console.log(process.env['BD_USER'])
-console.log(process.env['BD_PASSWORD']);
-
 const db = mongojs(`mongodb://${process.env['BD_USER']}:${process.env['BD_PASSWORD']}@ds121674.mlab.com:21674/mytasklismean`, ['tasks'])
 
+// get all tasks
 router.get('/tasks', (req, res, next) => {
     db.tasks.find( (err, tasks) => {
         if(err){
@@ -16,4 +14,31 @@ router.get('/tasks', (req, res, next) => {
     });
 });
 
+// get one task
+router.get('/task/:id', (req, res, next) => {
+    db.tasks.findOne( {_id: mongojs.ObjectId(req.params.id)}, (err, task) => {
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+// save task
+router.post('/task', (req, res, next) => {
+    var task = req.body;
+    if(!task.title || (task.isDone + '')) {
+        res.status(400);
+        res.json({
+            "error":"Bad Data"
+        });
+    } else {
+        db.tasks.save(task, (err, task) => {
+            if(err){
+                res.send(err);
+            }
+            res.json(task);
+        })
+    }
+});
 module.exports = router;
