032f942a9aa6fb9ba52c5b6851edce8f680dc0c7
kevin hu <hu.kevin.w@gmail.com>
2017-09-04 16:32:17 -0400

    Add function to call db

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const mongojs = require('mongojs');
+
+console.log(process.env['BD_USER'])
+console.log(process.env['BD_PASSWORD']);
+
+const db = mongojs(`mongodb://${process.env['BD_USER']}:${process.env['BD_PASSWORD']}@ds121674.mlab.com:21674/mytasklismean`, ['tasks'])
 
 router.get('/tasks', (req, res, next) => {
-    res.send('TASKS PAGE');
+    db.tasks.find( (err, tasks) => {
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
 });
 
 module.exports = router;
